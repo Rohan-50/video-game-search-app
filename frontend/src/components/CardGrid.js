fbad2e52b82bd551709484e6a42475fdb348ef24
Rohan Majeed <rohan.majeed@credera.com>
2023-12-21 17:05:11 -0600

    Added home page info cards

--- frontend/src/components/CardGrid.js
+++ frontend/src/components/CardGrid.js
@@ -1,47 +1,62 @@
 import React from 'react';
 import { Card, CardContent, CardMedia, Typography, Grid } from '@mui/material';
+import GameCollageImage from '../images/Game_Collage.jpg'
+import SearchFeatureImage from '../images/Search_Feature.png'
+import GameListImage from '../images/Game_List.jpeg'
 
 const cardData = [
-  {
-    id: 1,
-    image: 'image1.jpg', // Replace with actual image URL
-    heading: 'Card 1 Heading',
-    subheading: 'Card 1 Subheading',
-    description: 'Description for Card 1.',
-  },
-  {
-    id: 2,
-    image: 'image2.jpg', // Replace with actual image URL
-    heading: 'Card 2 Heading',
-    subheading: 'Card 2 Subheading',
-    description: 'Description for Card 2.',
-  },
-  {
-    id: 3,
-    image: 'image3.jpg', // Replace with actual image URL
-    heading: 'Card 3 Heading',
-    subheading: 'Card 3 Subheading',
-    description: 'Description for Card 3.',
-  },
+    {
+        id: 1,
+        image: GameCollageImage,
+        heading: 'ALL IN ONE PLACE',
+        subheading: 'A large collection of video games',
+        description: 'Access a full database of video games where you can filter \
+        (platform, tags, publishers, genres) and sort (release date, A-Z, ratings) \
+        the entire gaming catalog of over 9,000 games!',
+    },
+    {
+        id: 2,
+        image: SearchFeatureImage,
+        heading: 'NEW SEARCH FEATURES',
+        subheading: 'Use natural language to search',
+        description: 'A new feature (still being tested) is available that allows you to \
+        search for games using natural language. Now you can find that perfect game without \
+        having to spend time playing with filters.',
+    },
+    {
+        id: 3,
+        image: GameListImage,
+        heading: 'MANAGE MULTIPLE LISTS',
+        subheading: 'Create lists for all your gaming needs',
+        description: 'You can now create as many lists as you want to manage your gaming \
+        backlog. Whether you have a list of games that you still need to play, or have a \
+        list of favorites that you enjoy playing with friends, our lists can help with it all.',
+    },
 ];
 
 const CardGrid = () => {
-  return (
-    <Grid container spacing={30} justifyContent="center">
-      {cardData.map((card) => (
-        <Grid item key={card.id} sx={{display: 'flex'}}>
-          <Card style={{ width: '240px', height: '540px' }}>
-            <CardMedia component="img" height="140" image={card.image} alt={card.heading} />
-            <CardContent>
-              <Typography variant="h6">{card.heading}</Typography>
-              <Typography variant="subtitle1" color="textSecondary">{card.subheading}</Typography>
-              <Typography variant="body2">{card.description}</Typography>
-            </CardContent>
-          </Card>
+    return (
+        <Grid container spacing={10} justifyContent="center">
+            {cardData.map((card) => (
+                <Grid item key={card.id} sx={{ display: 'flex' }}>
+                    <Card sx={{ marginBottom: '50px', width: '40vh', backgroundColor: '#151515' }}>
+                        <CardMedia component="img" height="50%" width="50%" image={card.image} alt={card.heading} />
+                        <CardContent>
+                            <Typography variant="h5" sx={{ color: 'white', marginBottom: '30px' }}>
+                                {card.heading}
+                            </Typography>
+                            <Typography variant="h6" sx={{ color: 'white', marginBottom: '30px' }}>
+                                {card.subheading}
+                            </Typography>
+                            <Typography variant="body2" sx={{ color: 'white'}}>
+                                {card.description}
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                </Grid>
+            ))}
         </Grid>
-      ))}
-    </Grid>
-  );
+    );
 };
 
 export default CardGrid;
