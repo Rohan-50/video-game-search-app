be98afb1045b955f95b7e23143fcbdfb9e758799
Rohan Majeed <rohan.majeed@credera.com>
2024-01-02 16:35:35 -0600

    Finished data cleanup

--- cleanup.py
+++ cleanup.py
@@ -6,7 +6,7 @@
 # import openai
 # import os
 # import requests
-import json
+# import json
 
 # getting video game data from API
 """ api_url = "https://api.rawg.io/api/games?key=b299f11fbcf546f582963c5841705189&platforms=187,18,1,186,7&stores=2,3,6&dates=2019-01-01,2024-12-31&page_size=100000&page=1"
@@ -42,90 +42,18 @@ for game in game_data:
         print(f"Error fetching API data for game {game['id']}: {e}")
 
 with open('game_details.json', 'w', encoding='utf-8') as f:
-    json.dump(game_data, f, ensure_ascii=False, indent=4) """
-
-# Specify the path to your JSON file
-json_file_path = 'game_details.json'
-with open(json_file_path, 'r') as file:
-    game_data = json.load(file)
-
-counter = 1
-
-for game in game_data:
-    game.pop('slug', None)
-    game.pop('playtime', None)
-    game.pop('tba', None)
-    game.pop('suggestions_count', None)
-    game.pop('score', None)
-    game.pop('clip', None)
-    game.pop('user_game', None)
-    game.pop('parent_platforms', None)
-    game.pop('screenshots_count', None)
-    game.pop('movies_count', None)
-    game.pop('creators_count', None)
-    game.pop('achievements_count', None)
-    game.pop('parent_achievements_count', None)
-    game.pop('reddit_name', None)
-    game.pop('reddit_description', None)
-    game.pop('reddit_logo', None)
-    game.pop('reddit_count', None)
-    game.pop('twitch_count', None)
-    game.pop('youtube_count', None)
-    game.pop('alternative_names', None)
-    game.pop('parents_count', None)
-    game.pop('additions_count', None)
-    game.pop('game_series_count', None)
-    game.pop('description_raw', None)
-    if 'platforms' in game:
-        for platform in game['platforms']:
-            if 'platform' in platform:
-                platform['platform'].pop('slug', None)
-                platform['platform'].pop('image', None)
-                platform['platform'].pop('year_end', None)
-                platform['platform'].pop('year_start', None)
-                platform['platform'].pop('games_count', None)
-                platform['platform'].pop('image_background', None)
-                platform.pop('released_at', None)
-                platform.pop('requirements', None)
-    if 'stores' in game:
-        for store in game['stores']:
-            if 'store' in store:
-                store['store'].pop('slug', None)
-                store['store'].pop('games_count', None)
-                store['store'].pop('image_background', None)
-                store.pop('url', None)
-    if 'tags' in game:
-        for tag in game['tags']:
-            tag.pop('slug', None)
-            tag.pop('language', None)
-            tag.pop('games_count', None)
-            tag.pop('image_background', None)
-    if 'genres' in game:
-        for genre in game['genres']:
-            genre.pop('slug', None)
-            genre.pop('games_count', None)
-            genre.pop('image_background', None)
-    if 'metacritic_platforms' in game:
-        for platform in game['metacritic_platforms']:
-            if 'platform' in platform:
-                platform['platform'].pop('platform', None)
-                platform['platform'].pop('slug', None)
-    if 'developers' in game:
-        for developer in game['developers']:
-            developer.pop('slug', None)
-            developer.pop('games_count', None)
-            developer.pop('image_background', None)
-    if 'publishers' in game:
-        for publisher in game['publishers']:
-            publisher.pop('slug', None)
-            publisher.pop('games_count', None)
-            publisher.pop('image_background', None)
-    print("[" + str(counter) + "] " + str(game['name']) + " updated")
-    counter += 1
-
-with open('game_data.json', 'w', encoding='utf-8') as f:
     json.dump(game_data, f, ensure_ascii=False, indent=4)
 
+# Specify the path to your JSON file
+
+for game in game_data:
+    if 'stores' in game:
+        for store in game['stores']:
+            store.pop('id', None)
+        for store_info in game['stores']:
+            store_info.update(store_info.pop('store'))
+    print("[" + str(counter) + "] " + str(game['name']) + " updated")
+    counter += 1 """
 
 # testing HowLongToBeat API
 # results = HowLongToBeat().search(game_data[0]['name'])
