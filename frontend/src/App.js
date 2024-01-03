8fdac8f0b0e68298aae693070e9bd525dba5e53f
Rohan Majeed <rohan.majeed@credera.com>
2024-01-03 17:28:28 -0600

    Created home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,49 +1,17 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
-import ImageGallery from './components/ImageGallery';
-import CardGrid from './components/CardGrid';
-import Navbar from './components/Navbar';
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-import { Stack, Button, Typography } from '@mui/material';
+// App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import Games from './pages/Games';
 
 function App() {
-  const [message, setMessage] = useState('');
-
-  useEffect(() => {
-    fetch('/api')
-      .then(response => response.json())
-      .then(data => setMessage(data.message))
-      .catch(error => console.error('Error fetching data:', error));
-  }, []);
-  
-  const buttonStyle = {
-    height: '50px',
-    textTransform: 'none',
-  };
-
   return (
-    <div className="App">
-      <Navbar />
-        <Typography variant="h3" sx={{ marginTop: "200px", marginBottom: "50px", fontWeight: "600" }}>
-          Your guide for finding the perfect game
-        </Typography>
-        <Stack direction="row" spacing={2} sx={{ justifyContent: 'center', marginBottom: '200px'}}>
-          <Button variant="contained" color='success' size='large' style={buttonStyle}>
-            Discover Games
-          </Button>
-          <Button variant="outlined" size='large' style={buttonStyle}>
-            Search Feature
-          </Button>
-        </Stack>
-        <Typography variant="h6" sx={{color: 'gray', marginBottom: '20px'}}>
-          Platforms available on JustPlay
-        </Typography>
-        <ImageGallery/>
-        <CardGrid/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" exact element={<HomePage/>} />
+        <Route path="/games" element={<Games/>} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Stack, Button, Typography } from '@mui/material';
+import Navbar from '../components/Navbar';
+import ImageGallery from '../components/ImageGallery';
+import CardGrid from '../components/CardGrid';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+const HomePage = () => {
+  const buttonStyle = {
+    height: '50px',
+    textTransform: 'none',
+  };
+
+  return (
+    <div style={{textAlign: 'center', backgroundColor: 'black', color: 'white'}}>
+      <Navbar />
+      <Typography variant="h3" sx={{ marginTop: '200px', marginBottom: '50px', fontWeight: '600' }}>
+        Your guide for finding the perfect game
+      </Typography>
+      <Stack direction="row" spacing={2} sx={{ justifyContent: 'center', marginBottom: '200px' }}>
+        <Button variant="contained" color="success" size="large" style={buttonStyle}>
+          Discover Games
+        </Button>
+        <Button variant="outlined" size="large" style={buttonStyle}>
+          Search Feature
+        </Button>
+      </Stack>
+      <Typography variant="h6" sx={{ color: 'gray', marginBottom: '20px' }}>
+        Platforms available on JustPlay
+      </Typography>
+      <ImageGallery />
+      <CardGrid />
+    </div>
+  );
+};
+
+export default HomePage;
